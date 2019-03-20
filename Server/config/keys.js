2b98a159e5aae31746740251b7808b7a689d2be3
VSSL13 <vssl13_01@VSSL13s-MacBook-Pro.local>
2019-03-20 14:03:42 -0700

    added google id keys

---
+++ Server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '35305814190-1c3vr6fm2a4g5sestmnpbi71te96f5cf.apps.googleusercontent.com'
+    clientSecret: 'Ir9xhxk0YscoE6lPs_79e7NG'
+}
--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+//clientID 35305814190-1c3vr6fm2a4g5sestmnpbi71te96f5cf.apps.googleusercontent.com
+//clientSecret Ir9xhxk0YscoE6lPs_79e7NG
 passport.use(new GoogleStrategy());
 
 
