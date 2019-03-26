7e3fa7aafc616c79f183fff0327315306880a9ac
Damon Brown <dbrow437@gmail.com>
2019-03-25 20:42:01 -0700

    google oauth flow complete

--- Server/services/passport.js
+++ Server/services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
+});
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
@@ -16,7 +27,8 @@ passport.use(new GoogleStrategy({
                 if (existingUser) {
                     done(null, existingUser);
                 } else {
-                    new User({ googleId: profile.id }).save()
+                    new User({ googleId: profile.id })
+                        .save()
                         .then(user => done(null, user));
                 }
             })
