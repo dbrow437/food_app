ee716735d4034e780068f6803480d27550827a92
Damon Brown <dbrow437@gmail.com>
2019-03-31 20:31:01 -0700

    facebook oauth started

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+
+    // google 
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
         })
@@ -16,5 +18,14 @@ module.exports = (app) => {
     app.get('/api/current_user', (req, res) => {
         res.send(req.user)
     });
+
+    // facebook
+    app.get('/auth/facebook', passport.authenticate('facebook', {
+        scope: ['profile', 'email']
+        })
+    );
+    
+    app.get('/auth/facebook/callback', passport.authenticate('facebook'));
+
 }
 
